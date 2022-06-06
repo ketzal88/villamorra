c6e7a6dd70ca4b7bb32a195db3c47066da1b924b
ari-jakubowicz <94JA40126577@campus.economicas.uba.ar>
2022-06-06 16:07:51 +0300

    ajustes header

--- src/components/Header/header.styles.js
+++ src/components/Header/header.styles.js
@@ -80,7 +80,7 @@ export const useHeaderStyles = makeStyles((theme) => {
         fontSize: "14px",
       },
       [theme.breakpoints.up("lg")]: {
-        marginTop: "12px",
+        marginTop: "23px",
         paddingRight: "24px",
         fontSize: "17px",
       },
