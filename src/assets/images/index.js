e43e603fc26394b2d26558e642b564fdc3e5a063
clod2008 <claudio@apsis.com.ar>
2022-06-13 13:55:10 -0300

    apatamento Iconos estáticos

---
+++ src/assets/images/index.js
@@ -0,0 +1,8 @@
+export { default as hoja_1 } from "./hoja_1.svg";
+export { default as hoja_2 } from "./hoja_2.svg";
+export { default as hoja_3 } from "./hoja_3.svg";
+export { default as hoja_4 } from "./hoja_4.svg";
+export { default as sol_1 } from "./sol_1.svg";
+export { default as sol_2 } from "./sol_2.svg";
+export { default as sol_3 } from "./sol_3.svg";
+export { default as sol_4 } from "./sol_4.svg";
